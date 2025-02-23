5f8c7c1f7c128d7a12f9d7ec20e1b4e17f5c36e8
Asim Mulla <asimmulla2004@gmail.com>
2025-02-23 21:57:50 +0530

    DB url exposed

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(`${process.env.ATLAS_DB_URL}`)
+    .connect(
+      "mongodb+srv://AsimMulla:ejf64f8wj29fuw8eo3cjid@cluster0.kkh7x.mongodb.net/FoodRush"
+    )
     .then(() => console.log("DB Connected"));
 };
